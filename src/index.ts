8a5ce34399f755f542f9bccc5923374893ed428c
Allusive <auraud.zarafshar@gmail.com>
2021-12-22 22:16:19 -0800

    5 Hours

---
+++ src/index.ts
@@ -0,0 +1,96 @@
+import axios from "axios";
+import express from "express";
+import path from "path";
+import qs from "qs";
+require("dotenv").config();
+const clientId = process.env.CLIENT_ID;
+import SpotifyWebApi from "spotify-web-api-node";
+const clientSecret = process.env.CLIENT_SECRET;
+const redirectUri = "http://localhost:3000/callback";
+let token = "";
+const app = express();
+const scopes = [
+  // Allow all available scopes
+  "playlist-modify-private",
+  "playlist-read-collaborative",
+  "playlist-read-private",
+  "playlist-modify-public",
+  "user-follow-modify",
+  "user-follow-read",
+  "user-read-private",
+  "user-read-email",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-library-modify",
+  "user-library-read",
+  "user-read-playback-position",
+  "user-top-read",
+  "user-read-recently-played",
+];
+app.get("/", (req, res) => {
+  // Server index.html
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/auth", (req, res) => {
+  res.redirect(
+    `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`
+  );
+});
+
+app.get("/callback", (req, res) => {
+  const code: any = req.query.code;
+  // Convert code to access token
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString(
+      "base64"
+    )}`,
+  };
+
+  const url = "https://accounts.spotify.com/api/token";
+  const data = qs.stringify({
+    code: code,
+    redirect_uri: redirectUri,
+    grant_type: "authorization_code",
+  });
+
+  // Post request to get access token using axios
+  axios
+    .post(url, data, {
+      headers: headers,
+    })
+    .then((response) => {
+      token = response.data.access_token;
+      res.redirect(`/user?token=${token}`);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+app.get("/user", async (req, res) => {
+  const code: any = req.query.token;
+  var spotifyApi = new SpotifyWebApi({
+    clientId: clientId,
+    clientSecret: clientSecret,
+    redirectUri: redirectUri,
+  });
+  spotifyApi.setAccessToken(code);
+  if (code) {
+    const data = await spotifyApi.getMe();
+    spotifyApi.getUserPlaylists(data.display_name).then(
+      function (data) {
+        console.log("Retrieved playlists", data.body);
+      },
+      function (err) {
+        console.log("Something went wrong!", err);
+      }
+    );
+  }
+  res.send("Bad");
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!");
+});
